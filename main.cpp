90c48bff551866850548edcda7fe311a83de683d
fayvori <ignat.belousov2005@gmail.com>
2022-01-27 21:00:25 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    for (int case_num = 1; case_num <= T; case_num++) {
+        int S, N; cin >> S >> N;
+        std::queue< pair<int, int> > q1;
+
+
+
+    }
+
+    return 0;
+}
